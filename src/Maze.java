c0ee55cf610b9003af309fb70c29cd2a9b2df47b
nicholas hays <nicholas.hays@hotmail.com>
2016-03-02 22:43:46 -0800

    build source path

--- src/Maze.java
+++ src/Maze.java
@@ -1,3 +1,19 @@
 public class Maze {
-
+	/**
+	 * Creates a 2d maze of size n by m and with the debug flag set to true it 
+	 * will show the steps of maze creation. 
+	 * @param width the width of the maze 
+	 * @param depth the depth of the maze
+	 * @param debug the value of debug
+	 */
+	public Maze(int width, int depth, boolean debug){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 }
