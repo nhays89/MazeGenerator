2e2f7ad8704318f57ef44ef5f38d30f280103d97
nicholas hays <nicholas.hays@hotmail.com>
2016-03-04 14:22:09 -0800

    implemented buildMaze(), revalidate(), and createMaze()
    buildMaze() does not yet include random location traversal
    revalidate() will merge with canPass()
    createMaze() initializes the maze before construction
    
    //to do 
    randomize()
    include an arraylist to track shorteest path
    printShortestPath()

--- src/Maze.java
+++ src/Maze.java
@@ -1,12 +1,19 @@
-
+import java.util.ArrayList;
+/**
+ * 
+ * @author Nicholas Hays & Ethan Rowell
+ *
+ */
 public class Maze {
 	
 	private String[][] myMaze;
 	private int myWidth;
 	private int myDepth;
+	private int[] sourceCoords; // represent the starting loc
+	private int[] destinationCoords; //represent the ending loc
 	
 	/**
-	 * Creates a 2d maze of size n by m and with the debug flag set to true it 
+	 * Creates a 2d maze of size n by m. With the debug flag set to true it 
 	 * will show the steps of maze creation. 
 	 * @param width the width of the maze 
 	 * @param depth the depth of the maze
@@ -17,21 +24,192 @@ public class Maze {
 		myDepth = depth;
 		myMaze = new String[myWidth * 2 + 1][myDepth * 2 + 1];
 		createMaze();
-	
+		init();
+		printMaze();
+		System.out.println();
+		buildMaze(sourceCoords[0], sourceCoords[1]);
+		replaceShortestPath();
+		//printMaze();
+	}
+	private void replaceShortestPath() {
+		
+	}
+	/**
+	 * 
+	 * @param r
+	 * @param c
+	 */
+	private void buildMaze(int r, int c) {
+		if(!canPass(r,c)) return;
+		int[] myCurrentCoords = {r,c};
+		System.out.println("current loc: " + "(" + myCurrentCoords[0] + "," + myCurrentCoords[1] + ")");
+		ArrayList<int[]> myCurrentList = getValidLocs(r, c);
+		System.out.println("myCurrentList: " + "(" + myCurrentList.get(0)[0] + "," + myCurrentList.get(0)[1] + ")");
+		System.out.println("myCurrenList size : " + myCurrentList.size());
+		for(int i = 0; i < myCurrentList.size(); i++) {
+			if(revalidate(myCurrentCoords[0], myCurrentCoords[1])) {
+				int difference;
+				if(myCurrentCoords[0] == myCurrentList.get(i)[0]) {// if the current location's row == next location to visits row 
+					if(myCurrentCoords[1] > myCurrentList.get(i)[1]) {
+						difference = myCurrentCoords[1] -1;
+					} else {
+						difference = myCurrentCoords[1] + 1;
+					}
+					System.out.println(difference);
+					myMaze[myCurrentList.get(i)[0]][difference] = " ";
+					
+				} else {
+					if(myCurrentCoords[0] > myCurrentList.get(i)[0]) {
+						difference = myCurrentCoords[0] -1;
+					} else {
+						difference = myCurrentCoords[0] + 1;
+					}
+					myMaze[difference][myCurrentList.get(i)[1]] = " "; //else the current coords col == current list col
+				}
+				System.out.println("location chose: " + "(" + myCurrentList.get(i)[0] + "," + myCurrentList.get(i)[1] + ")");
+				myMaze[myCurrentList.get(i)[0]][myCurrentList.get(i)[1]] = "V";
+				printMaze();
+				System.out.println();
+				buildMaze(myCurrentList.get(i)[0], myCurrentList.get(i)[1]);
+			}
+		}
+	}
+
+	private boolean revalidate(int r, int c) {
+		boolean canPass = false;
+		// north
+		if (r - 2 > 0 && myMaze[r - 2][c] == " ") {
+			canPass = true;
+		}
+		//west
+		if (c + 2 < myMaze[r].length -1 && myMaze[r][c + 2] == " ") {
+			canPass = true;
+		} 
+		//south
+		if (r + 2 < myMaze.length -1 && myMaze[r + 2][c] == " ") {
+			canPass = true;
+		} 
+		//east
+		if (c - 2 > 0 && myMaze[r][c - 2] == " ") {
+			canPass = true;
+		}
+		System.out.println("revalidate: " + canPass);
+		return canPass;
+	}
+
+
+
+
+
+	private boolean canPass(int r, int c) {
+		boolean canPass = false;
+		// north
+		if (r - 2 > 0 && myMaze[r - 2][c] == " ") {
+			canPass = true;
+		}
+		//west
+		if (c + 2 < myMaze[r].length -1 && myMaze[r][c + 2] == " ") {
+			canPass = true;
+		} 
+		//south
+		if (r + 2 < myMaze.length -1 && myMaze[r + 2][c] == " ") {
+			canPass = true;
+		} 
+		//east
+		if (c - 2 > 0 && myMaze[r][c - 2] == " ") {
+			canPass = true;
+		}
+		System.out.println("can pass: " + canPass);
+		return canPass;
 	}
 	
 	
-	
+	private ArrayList<int[]> getValidLocs(int row, int col) {
+		ArrayList<int[]> validLocs = new ArrayList<>();
+		
+		
+		// checks North
+		if (row - 2 > 0 && myMaze[row - 2][col] == " ") {
+			int[] coordinates = {row - 2, col};
+			validLocs.add(coordinates);	
+		}
+		
+		// checks East
+		if (col + 2 < myWidth * 2 + 1 && myMaze[row][col + 2] == " ") {
+			int[] coordinates = {row, col + 2};
+			validLocs.add(coordinates);
+		
+		}
+		
+		// checks South
+		if (row + 2 < myDepth * 2 + 1 && myMaze[row + 2][col] == " ") {
+			int[] coordinates = {row + 2, col};
+			validLocs.add(coordinates);
+		}
+		
+		// checks West
+		if (col - 2 > 0 && myMaze[row][col - 2] == " ") {
+			int[] coordinates = {row, col - 2};
+			validLocs.add(coordinates);
+		}
+		
+		randomizeLocs(validLocs);
+		
+		for( int i= 0; i < validLocs.size(); i++) {
+			System.out.println("valid locs: " + "(" + validLocs.get(i)[0] + "," + validLocs.get(i)[1] + ")");
+		}
+		return validLocs;
+	}
+
+	private void randomizeLocs(ArrayList<int[]> validLocs) {
+		// TODO Auto-generated method stub
+		
+	}
+	private void init() {
+		sourceCoords = new int[2];
+		destinationCoords = new int[2];
+		sourceCoords[0] = 1;
+		sourceCoords[1] = 1;
+		destinationCoords[0] = myMaze.length -1;
+		destinationCoords[1] = myMaze.length -2;
+		myMaze[0][1] = " ";
+		myMaze[1][1] = "V";
+		myMaze[myMaze.length -1][myMaze.length -2] = " ";
+	}
+
+
+
+
+
 	private void createMaze(){
-		System.out.println(myMaze.length);
-		/*for(int i = 0; i < myMaze.length; i++) {
-			for(int j = 0; j < )
-		}*/
+		for(int i = 0; i < myMaze.length; i++) {
+			for(int j = 0; j < myMaze[i].length; j ++) {
+				if( i % 2 == 0) {
+					myMaze[i][j] = "X";
+				} else {
+					if(j % 2 == 0){
+						myMaze[i][j] = "X";
+					} else {
+						myMaze[i][j] = " ";
+					}
+				}
+			}
+		}
 	}
 	
 	
 	
 	
+	
+	private void printMaze() {
+		for(int i = 0; i < myMaze.length; i++) {
+			System.out.print("\n");
+			for(int j = 0; j < myMaze[i].length; j ++) {
+					System.out.print(myMaze[i][j]);
+			}
+		}
+	}
+
 	
 	
 	
@@ -97,44 +275,7 @@ public class Maze {
 	}
 	
 	
-	private int[][] getValidLocs(int row, int col) {
-		int[][] validLocs = new int[4][4];
-		int arrIndex = 0;
-		
-		// checks North
-		if (row - 2 > 0 && myMaze[row - 2][col] == " ") {
-			validLocs[arrIndex][0] = row - 2;
-			validLocs[arrIndex][1] = col;
-			arrIndex++;
-		}
-		
-		// checks East
-		if (col + 2 < myWidth * 2 + 1 && myMaze[row][col + 2] == " ") {
-			validLocs[arrIndex][0] = row;
-			validLocs[arrIndex][1] = col + 2;
-			arrIndex++;
-		}
-		
-		// checks South
-		if (row + 2 < myDepth * 2 + 1 && myMaze[row + 2][col] == " ") {
-			validLocs[arrIndex][0] = row + 2;
-			validLocs[arrIndex][1] = col;
-			arrIndex++;
-		}
-		
-		// checks West
-		if (col - 2 > 0 && myMaze[row][col - 2] == " ") {
-			validLocs[arrIndex][0] = row;
-			validLocs[arrIndex][1] = col - 2;
-			arrIndex++;
-		}
-		
-		// randomizes locations
-		
-		
-		return validLocs;
-	}
-	
+
 	
 	
 	/*
