24a127c660f71b2d4da02db0ebe1489b21fc2d38
nicholas hays <nicholas.hays@hotmail.com>
2016-03-05 01:51:01 -0800

    completed testing of maze generator, I still have yet to make view.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
+import java.util.Random;
 
+/**
+ * Main controller for testing mazes. 
+ * 
+ * @author Nicholas Hays & Ethan Rowell
+ * @version March 5, 2016
+ * Assignment 5: Maze Generator
+ * Presented For: Dr. Chris Marriott
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		Maze maze = new Maze(30,30, false);
+		Random rand = new Random();
+		
+		/* tests random mazes */
+		for (int i = 0; i < 5; i++) {
+			testSize(rand.nextInt(15) + 1, rand.nextInt(15) + 1, rand.nextBoolean());
+			
+			System.out.println();
+		}
+		
+		/* tests display method */
+		Maze testMaze = new Maze(rand.nextInt(10) + 1, rand.nextInt(10) + 1, false);
+		System.out.println("tests display");
+		testMaze.display();
+	}
+
+	public static void testSize(int depth, int width, boolean debug) {
+		System.out.println("Tests a " + depth + " x " + width + " maze with debug set to " + debug);
+		new Maze(depth, width, debug).display();
+		
 	}
 
 }
--- src/Maze.java
+++ src/Maze.java
@@ -1,26 +1,61 @@
-
-/**
- * 
- */
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Stack;
 
 /**
+ * Model for generating random maze with equal likelihood of occurrence.
  * 
  * @author Nicholas Hays & Ethan Rowell
- *
+ * @version March 5, 2016
+ * Assignment 5: Maze Generator
+ * Presented For: Dr. Chris Marriott
  */
 public class Maze {
-
+	
+	/**
+	 * The two-dimensional maze of characters. 
+	 */
 	private String[][] myMaze;
+	
+	/**
+	 * The width (number of columns) of the maze. 
+	 */
 	private int myWidth;
+	
+	/**
+	 * The depth (number of rows) of the maze. 
+	 */
 	private int myDepth;
-	private int[] sourceCoords; // represent the starting loc
-	private int[] destinationCoords; // represent the ending loc
+	
+	/**
+	 * The starting coordinates of the maze search. 
+	 */
+	private int[] sourceCoords; 
+	
+	/**
+	 * The ending coordinates of the maze search. 
+	 */
+	private int[] destinationCoords; 
+	
+	/**
+	 * Stack of coordinates of the shortest path from source to destination. 
+	 */
 	private Stack<int[]> shortestPathLocs;
+	
+	/**
+	 * Stack of all visited coordinates of the maze. 
+	 */
 	private Stack<int[]> visitedLocs;
+	
+	/**
+	 * Determines if the search has reached the destination coordinate. 
+	 */
 	private boolean reachedDestination;
+	
+	/**
+	 * Determines if the debug mode is set. 
+	 */
+	private boolean debugOn;
 
 	/**
 	 * Creates a 2d maze of size n by m. With the debug flag set to true it will
@@ -33,64 +68,79 @@ public class Maze {
 	public Maze(int depth, int width, boolean debug) {
 		myWidth = width;
 		myDepth = depth;
+		debugOn = debug;
 		myMaze = new String[myDepth * 2 + 1][myWidth * 2 + 1];
 		shortestPathLocs = new Stack<int[]>();
 		visitedLocs = new Stack<int[]>();
 		createMaze();
 		init();
-		//printMaze();
 		buildMaze(sourceCoords[0], sourceCoords[1]);
 		replaceMazeChars();
-		printMaze();
-	}
-
-	private void replaceMazeChars() {
-		Stack<int[]> tempStack = new Stack<>();
-		while (!visitedLocs.isEmpty()) {
-			boolean isFound = false;
-			int[] vLocs = visitedLocs.pop();
-			while (!shortestPathLocs.isEmpty()) {
-				int[] sLocs = shortestPathLocs.pop();
-				if (vLocs[0] == sLocs[0] && vLocs[1] == sLocs[1]) {
-					myMaze[sLocs[0]][sLocs[1]] = "+";
-					isFound = true;
-					break;
-				} else {
-					tempStack.push(sLocs);
-				}
-			}
-			if (!isFound) {
-				myMaze[vLocs[0]][vLocs[1]] = " ";
-			}
-			while (!tempStack.isEmpty()) {
-				shortestPathLocs.push(tempStack.pop());
-			}
-		}
 	}
 
 	/**
+	 * Initializes the characters of the maze. 
+	 */
+	private void createMaze() {
+		for (int i = 0; i < myDepth * 2 + 1; i++) {
+			for (int j = 0; j < myWidth * 2 + 1; j++) {
+				if (i % 2 == 0) {
+					myMaze[i][j] = "X";
+				} else {
+					if (j % 2 == 0) {
+						myMaze[i][j] = "X";
+					} else {
+						myMaze[i][j] = " ";
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Initializes source and destination coordinates.
+	 */
+	private void init() {
+		sourceCoords = new int[2];
+		destinationCoords = new int[2];
+		sourceCoords[0] = 1;
+		sourceCoords[1] = 1;
+		destinationCoords[0] = myDepth * 2 - 1;
+		destinationCoords[1] = myWidth * 2 - 1;
+		myMaze[0][1] = " ";
+		myMaze[1][1] = "V";
+		myMaze[myDepth * 2][myWidth * 2 - 1] = " ";
+	}
+
+	/**
+	 * Recursively traverses a 2 dimensional grid visiting each coordinate until a specified
+	 * destination cell has been visited. Keeps track of shortest path to destination coordinate
+	 * using stack by pushing each visited coordinate onto shortest path stack, and popping 
+	 * coordinate if a return from buildmaze(r,c) occurs while the destination coordinate has not yet
+	 * been visited.
 	 * 
-	 * @param r
-	 * @param c
+	 * @param r the row the start the traversal within the grid
+	 * @param c the column to start the traversal within the grid
 	 */
 	private void buildMaze(int r, int c) {
 		hasReachedDestination(r, c);
 		int[] visitedCoords = { r, c };
 		visitedLocs.push(visitedCoords);
-		if (!canPass(r, c)) {
+		if (!validate(r, c)) {
 			return;
 		}
+		ArrayList<int[]> myCurrentList = getValidLocs(r, c);
 		int[] myCurrentCoords = { r, c };
-		System.out.println();
 		if (!reachedDestination) {
 			int[] coord = { myCurrentCoords[0], myCurrentCoords[1] };
 			shortestPathLocs.push(coord);
 		}
-		ArrayList<int[]> myCurrentList = getValidLocs(r, c);
-		printMaze();
-		System.out.println();
+		if (debugOn) {
+			display();
+			System.out.println();
+		}
 		for (int i = 0; i < myCurrentList.size(); i++) {
-			if (revalidate(myCurrentCoords[0], myCurrentCoords[1])) {
+			if (validate(myCurrentCoords[0], myCurrentCoords[1])) {
 				int difference;
 				if (myCurrentCoords[0] == myCurrentList.get(i)[0]) {
 					if (myCurrentCoords[1] > myCurrentList.get(i)[1]) {
@@ -116,7 +166,14 @@ public class Maze {
 			shortestPathLocs.pop();
 		}
 	}
-
+	
+	/**
+	 * Determines if the row and column of the current coordinate being visited 
+	 * matches the destination row and column. 
+	 * 
+	 * @param r the row to check equality
+	 * @param c the column to check equality
+	 */
 	private void hasReachedDestination(int r, int c) {
 		if (r == destinationCoords[0] && c == destinationCoords[1]) {
 			int[] coord = { r, c };
@@ -124,8 +181,17 @@ public class Maze {
 			reachedDestination = true;
 		}
 	}
-
-	private boolean revalidate(int r, int c) {
+	
+	/**
+	 * Seeks to find neighboring coordinates surrounding the current 
+	 * coordinate that have not yet been visited. 
+	 * 
+	 * @param r the current row. 
+	 * @param c the current column.
+	 * @return a value indicating if the current coordinate has neighboring cells that
+	 * have not yet been visited. 
+	 */
+	private boolean validate(int r, int c) {
 		boolean canPass = false;
 		// north
 		if (r - 2 > 0 && myMaze[r - 2][c] == " ") {
@@ -145,28 +211,15 @@ public class Maze {
 		}
 		return canPass;
 	}
-
-	private boolean canPass(int r, int c) {
-		boolean canPass = false;
-		// north
-		if (r - 2 > 0 && myMaze[r - 2][c] == " ") {
-			canPass = true;
-		}
-		// west
-		if (c + 2 < myMaze[r].length - 1 && myMaze[r][c + 2] == " ") {
-			canPass = true;
-		}
-		// south
-		if (r + 2 < myMaze.length - 1 && myMaze[r + 2][c] == " ") {
-			canPass = true;
-		}
-		// east
-		if (c - 2 > 0 && myMaze[r][c - 2] == " ") {
-			canPass = true;
-		}
-		return canPass;
-	}
-
+	
+	/**
+	 * Provides a list of unvisited adjacent coordinates surrounding the current coordinate. 
+	 * 
+	 * @param row the current row
+	 * @param col the current column
+	 * @return list of available coordinates surrounding current cell in matrix that
+	 * have not yet been visited. 
+	 */
 	private ArrayList<int[]> getValidLocs(int row, int col) {
 		ArrayList<int[]> validLocs = new ArrayList<>();
 
@@ -198,43 +251,48 @@ public class Maze {
 		Collections.shuffle(validLocs);
 		return validLocs;
 	}
-
-	private void init() {
-		sourceCoords = new int[2];
-		destinationCoords = new int[2];
-		sourceCoords[0] = 1;
-		sourceCoords[1] = 1;
-		destinationCoords[0] = myDepth * 2 - 1;
-		destinationCoords[1] = myWidth * 2 - 1;
-		myMaze[0][1] = " ";
-		myMaze[1][1] = "V";
-		System.out.println(myMaze.length);
-		/* myMaze[myMaze.length - 1][myMaze.length - 2] = " "; */
-		myMaze[myDepth * 2][myWidth * 2 - 1] = " ";
-	}
-
-	private void createMaze() {
-		for (int i = 0; i < myDepth * 2 + 1; i++) {
-			for (int j = 0; j < myWidth * 2 + 1; j++) {
-				if (i % 2 == 0) {
-					myMaze[i][j] = "X";
+	
+	/**
+	 * Compares each coordinate on the visited location stack with each
+	 * coordinate on the shortest path stack to determine its respective
+	 * character representation.
+	 * If a coordinate is in the visited location stack but not in the shortest
+	 * path stack its symbol is a " " character, and if a coordinate
+	 * is in both stacks then it's symbol is a "+" character.  
+	 */
+	private void replaceMazeChars() {
+		Stack<int[]> tempStack = new Stack<>();
+		while (!visitedLocs.isEmpty()) {
+			boolean isFound = false;
+			int[] vLocs = visitedLocs.pop();
+			while (!shortestPathLocs.isEmpty()) {
+				int[] sLocs = shortestPathLocs.pop();
+				if (vLocs[0] == sLocs[0] && vLocs[1] == sLocs[1]) {
+					myMaze[sLocs[0]][sLocs[1]] = "+";
+					isFound = true;
+					break;
 				} else {
-					if (j % 2 == 0) {
-						myMaze[i][j] = "X";
-					} else {
-						myMaze[i][j] = " ";
-					}
+					tempStack.push(sLocs);
 				}
+			}
+			if (!isFound) {
+				myMaze[vLocs[0]][vLocs[1]] = " ";
+			}
+			while (!tempStack.isEmpty()) {
+				shortestPathLocs.push(tempStack.pop());
 			}
 		}
 	}
-
-	private void printMaze() {
+	
+	/**
+	 * Prints the contents of the current maze. 
+	 */
+	public void display() {
 		for (int i = 0; i < myMaze.length; i++) {
-			System.out.print("\n");
 			for (int j = 0; j < myMaze[i].length; j++) {
 				System.out.print(myMaze[i][j]);
 			}
+			System.out.print("\n");
 		}
 	}
 
